403b288cdc214a2bfac1d0a3b9b02066528b7bc6
Samuel Olaniyan <olaniyansamuel2015@gmail.com>
2022-02-24 19:10:05 +0000

    Skeleton crimemap

--- crimemap.py
+++ crimemap.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from dbhelper import DBHelper
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+DB = DBHelper()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    try:
+        data = DB.get_all_inputs()
+    except Exception as e:
+        print(e)
+        data = None
+    return render_template("home.html", data=data)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    try:
+        data = request.form.get("userinput")
+        DB.add_input(data)
+    except Exception as e:
+        print(e)
+    return home()
+
+
+@app.route("/clear")
+def clear():
+    try:
+        DB.clear_all()
+    except Exception as e:
+        print(e)
+    return home()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
